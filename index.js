73035424e1e8766be01b300c0f7716482b89ac6d
Ghulam shubhani <82954416+GhulamShubhani@users.noreply.github.com>
2023-08-06 19:41:20 +0530

    change config2

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ const corsOptions = {
     },
   };
   
-  app.use(cors(corsOptions));
-// app.use(cors())
+app.use(cors(corsOptions));
+app.use(cors())
 app.use(express.json())
 app.use("/user", AllUser);
 app.use("/playlist", MoviesPlayListRouter);
